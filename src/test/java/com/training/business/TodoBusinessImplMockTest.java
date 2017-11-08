d9346a5948ab3d7cd2eb364a0969566a3812e8ef
Deyan <dublinstand@gmail.com>
2017-11-08 14:22:12 +0000

    Initial

--- src/test/java/com/training/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/training/business/TodoBusinessImplMockTest.java
@@ -1,12 +1,14 @@
 package com.training.business;
 
 import com.training.data.api.TodoService;
-import com.training.data.api.TodoServiceStub;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TodoBusinessImplMockTest {
 
@@ -18,16 +20,48 @@ public class TodoBusinessImplMockTest {
 
     @Test
     public void testRetrieveTodosRelatedToSpring_usingAMock(){
-        //instantiate the TodoServiceStub that returns the interface
-        TodoService todoServiceStub = new TodoServiceStub();
+        //With mocking dynamically have the method return a given value
+
+        //Here we create a mock object of TodoService.class (we can mock also an interface)
+        TodoService todoServiceMock = mock(TodoService.class);
+
+        //This will be our data to be returned when the method is called
+        List<String> todos = Arrays.asList("Learn Spring MVC","Learn Spring", "Learn to Dance");
+
+        //Here we specify that when our mock service calls retrieveTodos and pass in "Dummy user"
+        //as user name, then we return our todos List
+        when(todoServiceMock.retrieveTodos("Dummy user")).thenReturn(todos);
 
         //we instantiate our TodoBusinsessImpl class that we want to test and pass the todoServiceStub we created
-        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceStub);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
 
-        //here we pass our stubbed TodoBusinessImpl that gets a list of 3 records from todoService.retrieveTodos(user)
+        //here we pass our mocked TodoBusinessImpl that gets a list of 3 records from todoService.retrieveTodos(user)
         //and two of them contain Spring, so filteredTodos will have a size of 2
         List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy user");
 
         assertEquals("We expect the result returned by our stub is 2", 2, filteredTodos.size());
+    }
+
+    @Test
+    public void testRetrieveTodosRelatedToSpring_withEmptyList(){
+        //With mocking dynamically have the method return a given value
+
+        //Here we create a mock object of TodoService.class (we can mock also an interface)
+        TodoService todoServiceMock = mock(TodoService.class);
+
+        //This will be our data to be returned when the method is called
+        //In this case we return an empty list
+        List<String> todos = Arrays.asList();
+
+        //Here we specify that when our mock service calls retrieveTodos and pass in "Dummy user"
+        //as user name, then we return our todos List
+        when(todoServiceMock.retrieveTodos("Dummy user")).thenReturn(todos);
+
+        //we instantiate our TodoBusinsessImpl class that we want to test and pass the todoServiceStub we created
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+        List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy user");
+
+        assertEquals(0, filteredTodos.size());
     }
 }
--- src/test/java/com/training/data/api/TodoServiceStub.java
+++ src/test/java/com/training/data/api/TodoServiceStub.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class TodoServiceStub implements TodoService {
     public List<String> retrieveTodos(String user) {
-        return Arrays.asList("Learn Spring MVC","Learn Spring", "Learn to DAnce");
+        return Arrays.asList("Learn Spring MVC","Learn Spring", "Learn to Dance");
     }
 
     public void deleteTodo(String todo) {

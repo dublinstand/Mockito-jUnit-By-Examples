43200a830c9e99a50133f8f6b2b4c3dd43216b19
Deyan <dublinstand@gmail.com>
2017-11-09 11:15:44 +0000

    Initial

--- src/test/java/com/training/mockito/HamcrestMatcherTest.java
+++ src/test/java/com/training/mockito/HamcrestMatcherTest.java
@@ -1,4 +1,39 @@
 package com.training.mockito;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class HamcrestMatcherTest {
+
+    @Test
+    public void hamcrestMatchersExamples(){
+        List<Integer> scores = Arrays.asList(99, 100, 101, 105);
+
+        //Scores has 4 items
+        assertThat(scores, hasSize(4));
+        assertThat(scores, hasItems(99, 100));
+
+        //Every item > 90
+        assertThat(scores, everyItem(greaterThan(90)));
+        assertThat(scores, everyItem(lessThan(190)));
+
+        //String
+        assertThat("", isEmptyString());
+        assertThat(null, isEmptyOrNullString());
+
+        //Arrays
+        Integer[] marks = {1, 2, 3};
+        
+        assertThat(marks, arrayWithSize(3));
+        assertThat(marks, arrayContaining(1,2,3));
+        assertThat(marks, arrayContainingInAnyOrder(1,2,3));
+
+    }
+
 }
+
